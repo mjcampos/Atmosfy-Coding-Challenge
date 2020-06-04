7b2ca124d8224cde2990c3fb2e8351129d77166d
Michael Campos <camposmj89@gmail.com>
2020-06-04 14:07:25 -0700

    Fleshed out the API for getting the local weather

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export var getWeather = ({lat, lon}) => {
-	console.log(lat);
-	console.log(lon);
 	return axios.get('https://fcc-weather-api.glitch.me/api/current', {
 		params: {
 			lat,
